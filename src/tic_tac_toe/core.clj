1dae96a0d09a248fa68942467c61970c2f636486
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2017-01-06 10:46:17 -0600

    Add player options to games
     * Console input will come in future commits

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,4 +1,5 @@
 (ns tic-tac-toe.core
-  (:require [tic-tac-toe.game :as g]))
+  (:require [tic-tac-toe.game :as g])
+  (:require [tic-tac-toe.board :as b]))
 
-(defn -main [] (g/play))
+(defn -main [] (g/play g/human g/dumb-computer b/empty-board))
