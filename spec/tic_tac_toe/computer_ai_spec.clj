911beaf6c0bbb1d88ddc021cd4bc7f7242b80621
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2017-01-11 10:43:34 -0600

    Minimax blocks winning moves by an opponent

--- spec/tic_tac_toe/computer_ai_spec.clj
+++ spec/tic_tac_toe/computer_ai_spec.clj
@@ -93,7 +93,19 @@
 
   (it "scores a winning move on a full board with a +1 (complementary board)"
       (let [current-board complementary-almost-tie-game-board]
-        (should= 1 (utility :player-one current-board 7)))))
+        (should= 1 (utility :player-one current-board 7))))
+
+  (it "scores a move as -1 if the opponent can win horizontally on the next turn"
+      (let [current-board (b/mark-many :player-one [1 2])]
+        (should= -1 (utility :player-two current-board 7))))
+
+  (it "scores a move as -1 if the opponent can win vertically on the next turn"
+      (let [current-board (b/mark-many :player-two [2 5])]
+        (should= -1 (utility :player-one current-board 1))))
+
+  (it "scores a move as -1 if the opponent can win diagonally on the next turn"
+      (let [current-board (b/mark-many :player-two [0 4])]
+        (should= -1 (utility :player-one current-board 1)))))
 
 (describe "utilities for remaining cells"
 
@@ -115,6 +127,44 @@
   (it "returns the correct output if the player can win in the left column"
       (let [expected-output [{1 nil} {2 nil} {3 1} {4 nil} {5 nil} {7 nil} {8 nil}]]
         (should= expected-output (utilities-for-remaining-cells :player-two (b/mark-many :player-two [6 0]))))))
+
+(describe "select best move"
+
+  (it "selects a winning move from a list"
+      (let [input [{0 nil} {1 nil} {2 nil} {3 nil} {4 nil} {5 1} {6 nil} {7 nil} {8 nil}]]
+        (should= 5 (best-move input))))
+
+  (it "selects a different winning move from a list"
+      (let [input [{0 nil} {1 1} {2 nil} {3 nil} {4 nil} {5 nil} {6 nil} {7 nil} {8 nil}]]
+        (should= 1 (best-move input))))
+
+  (it "returns the latest winning move"
+      (let [input [{0 nil} {1 1} {2 1} {3 1} {4 1} {5 nil} {6 nil} {7 1} {8 nil}]]
+        (should= 7 (best-move input))))
+
+  (it "ignores a tie if a win exists"
+      (let [input [{0 0} {1 nil} {2 nil} {3 nil} {4 nil} {5 nil} {6 nil} {7 0} {8 1}]]
+        (should= 8 (best-move input))))
+
+  (it "ignores a loss if a win exists"
+      (let [input [{0 nil} {1 nil} {2 1} {3 nil} {4 -1} {5 nil} {6 -1} {7 -1} {8 nil}]]
+        (should= 2 (best-move input))))
+
+  (it "selects a tie move from a list"
+    (let [input [{0 nil} {1 nil} {2 nil} {3 nil} {4 nil} {5 nil} {6 0} {7 nil} {8 nil}]]
+      (should= 6 (best-move input))))
+
+  (it "ignores a loss if a tie exists"
+      (let [input [{0 nil} {1 nil} {2 0} {3 nil} {4 -1} {5 nil} {6 -1} {7 -1} {8 nil}]]
+        (should= 2 (best-move input))))
+
+  (it "returns a space on the list, even if they are nil"
+      (let [input [{1 nil} {3 nil} {4 nil} {5 nil} {6 nil} {7 nil}]]
+        (should= 7 (best-move input))))
+
+  (it "avoids a loss as much as possible"
+      (let [input [{1 nil} {3 nil} {4 nil} {5 nil} {6 -1}]]
+        (should= 5 (best-move input)))))
 
 (describe "an unbeatable computer player"
 
@@ -151,4 +201,44 @@
   (it "takes a diagonal winning move when it is available"
       (let [current-board (b/mark-many :player-one [0 4])
             expected-output (b/mark-many :player-one [0 4 8])]
-        (should= expected-output (unbeatable-ai :player-one current-board)))))
+        (should= expected-output (unbeatable-ai :player-one current-board))))
+
+  (it "blocks an opponent who is about to win vertically"
+      (let [current-board (b/mark-many :player-one [0 3])
+            expected-output (b/mark :player-two 6 (b/mark-many :player-one [0 3]))]
+        (should= expected-output (unbeatable-ai :player-two current-board))))
+
+  (it "blocks an opponent who is about to win horizontally"
+      (let [current-board (b/mark-many :player-two [6 8])
+            expected-output (b/mark :player-one 7 (b/mark-many :player-two [6 8]))]
+        (should= expected-output (unbeatable-ai :player-one current-board))))
+
+  (it "blocks an opponent who is about to win diagonally"
+      (let [current-board (b/mark-many :player-two [4 6])
+            expected-output (b/mark :player-one 2 (b/mark-many :player-two [4 6]))]
+        (should= expected-output (unbeatable-ai :player-one current-board))))
+
+  (it "it is a fatalistic computer player"
+      (let [current-board (b/mark-many :player-one [0 1 3])
+            expected-output (b/mark :player-two 8 (b/mark-many :player-one [0 1 3]))]
+        (should= expected-output (unbeatable-ai :player-two current-board))))
+
+  (it "picks a winning spot if two spots are available"
+      (let [current-board (b/mark-many :player-one [0 1 3])
+            expected-output (b/mark :player-one 6 (b/mark-many :player-one [0 1 3]))]
+        (should= expected-output (unbeatable-ai :player-one current-board))))
+
+  (it "does not pick a losing spot on a fixed board"
+      (let [current-board (b/mark-many :player-one [0 1 3 4])
+            expected-output (b/mark :player-one 8 (b/mark-many :player-one [0 1 3 4]))]
+        (should= expected-output (unbeatable-ai :player-one current-board))))
+
+  (it "picks a winning spot over blocking a win (player one)"
+      (let [current-board (b/mark-many :player-two [6 7] (b/mark-many :player-one [3 4]))
+            expected-output (b/mark :player-one 5 (b/mark-many :player-two [6 7] (b/mark-many :player-one [3 4])))]
+        (should= expected-output (unbeatable-ai :player-one current-board))))
+
+  (it "picks a winning spot over blocking a win (player two)"
+      (let [current-board (b/mark-many :player-two [6 7] (b/mark-many :player-one [3 4]))
+            expected-output (b/mark :player-two 8 (b/mark-many :player-two [6 7] (b/mark-many :player-one [3 4])))]
+        (should= expected-output (unbeatable-ai :player-two current-board)))))
--- src/tic_tac_toe/computer_ai.clj
+++ src/tic_tac_toe/computer_ai.clj
@@ -6,18 +6,46 @@
   (let [spaces (b/remaining-spaces board)]
     (b/mark player (apply min spaces) board)))
 
+(defn- lazy-contains? [col key]
+  (= key (some #{key} col)))
+
+(defn- opponent [player]
+  (if (= player :player-one) :player-two :player-one))
+
+(defn- game-status-for-each-move [player board]
+  (loop [spaces (b/remaining-spaces board) statuses []]
+    (if (empty? spaces)
+      statuses
+      (recur (rest spaces) (conj statuses (r/game-status (b/mark player (first spaces) board)))))))
+
+(defn- player-can-win-this-turn [player board]
+  (let [statuses (game-status-for-each-move player board)]
+    (lazy-contains? statuses player)))
+
+(defn- eliminate-negative-utilities [moves-and-utilities]
+  (if (empty? (filter #(not (= (first (vals %1)) -1)) moves-and-utilities))
+    moves-and-utilities
+    (filter #(not (= (first (vals %1)) -1)) moves-and-utilities)))
+
+(defn- sorted-moves-and-utilities [moves-and-utilities]
+  (let [moves-without-negative-utility (eliminate-negative-utilities moves-and-utilities)]
+    (sort-by #(first (vals %1)) moves-without-negative-utility)))
+
 (defn utility [player board cell]
   (let [status (r/game-status (b/mark player cell board))]
     (cond
       (= status player) 1
       (= status :tie) 0
+      (and (= status :in-progress) (player-can-win-this-turn (opponent player) (b/mark player cell board))) -1
       :else nil)))
 
 (defn utilities-for-remaining-cells [player board]
   (map #(hash-map %1 (utility player board %1)) (b/remaining-spaces board)))
 
+(defn best-move [moves-and-utilities]
+  (let [extract-move (comp first keys last sorted-moves-and-utilities)]
+    (extract-move moves-and-utilities)))
+
 (defn unbeatable-ai [player board]
-  (let [utilities (utilities-for-remaining-cells player board)
-        default-spot (first (b/remaining-spaces board))
-        cell (or (first (keys (first (filter #(not (nil? (first (vals %1)))) utilities)))) default-spot)]
-      (b/mark player cell board)))
+  (let [moves-and-utilities (utilities-for-remaining-cells player board)]
+    (b/mark player (best-move moves-and-utilities) board)))
